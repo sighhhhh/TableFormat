6188fc62d6f90e4d55295db92a7f5385c2052d61
sighhh <sighhh0704@gmail.com>
2023-12-25 22:24:37 +0800

    优化查询逻辑

--- ExcelFormat.py
+++ ExcelFormat.py
@@ -4,12 +4,14 @@ import openpyxl
 import configparser
 from openpyxl.workbook import Workbook
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+config_file_path = os.path.join(script_dir,"Directory.ini")
 config = configparser.ConfigParser()
-config.read("Directory.ini")
+config.read(config_file_path)
 
 
 # 遍历多个excel文件
-def ReadDir(directory: string):
+def read_dir(directory: string):
     filename = []
     filenames = []
     print(directory)
@@ -22,30 +24,29 @@ def ReadDir(directory: string):
 
 
 # 遍历读取表格的每一个 sheet 表
-def ReadTable(files):
-    ListTemp = []
+def read_table(files):
+    list_temp = []
     for fileindex in files:
         book = openpyxl.load_workbook(fileindex)
         sheetNamelist = book.sheetnames
         # 遍历~一张 sheet 表
         for name in sheetNamelist:
             sheet = book[name]
-            print(sheet)
-            # 遍历读取每一个 sheet 表中的 每一列
-            for col in sheet.iter_cols():
-                # print(col)
-                # 若整列中含不为空的数据项，则保留数据
-                if all(cell.value is None for cell in col):
-                    continue
-                else:
-                    for cell in col:
-                        if cell.value is not None:
-                            ListTemp.append(cell.value)
-    return ListTemp
+            get_non_empty_cell_value(sheet,list_temp)
+    return list_temp
+
+def get_non_empty_cell_value(sheet,list_temp):
+    # 遍历读取每一个 sheet 表中的 每一列
+    for col in sheet.iter_cols():
+        # 若整列中含不为空的数据项，则保留数据
+        if any(cell.value is not None for cell in col):
+            for cell in col:
+                if cell.value is not None:
+                    list_temp.append(cell.value)
 
 
 # 填入临时表中
-def WriteTable(listtemp: list):
+def write_table(listtemp: list):
     WorkTable = Workbook()
     sheet = WorkTable.active
     sheet.title = 'Sheet1'
@@ -55,6 +56,6 @@ def WriteTable(listtemp: list):
 
 
 if __name__ == "__main__":
-    Directory = config.get("Directory", "ReadPath")
-    Files = ReadDir(Directory)
-    WriteTable(ReadTable(Files))
+    directory = config.get("Directory", "ReadPath")
+    files = read_dir(directory)
+    write_table(read_table(files))
